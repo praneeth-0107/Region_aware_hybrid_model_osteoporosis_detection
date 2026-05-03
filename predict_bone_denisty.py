d943950a41ba48c53e9d6eeac5dbe07252a6ea4b
praneeth-0107 <praneeth_potta@srmap.edu.in>
2026-05-03 20:43:10 +0530

    Make matplotlib/seaborn lazy imports - fixes Railway deploy crash

--- predict_bone_denisty.py
+++ predict_bone_denisty.py
@@ -35,11 +35,15 @@ import argparse
 import warnings
 import numpy as np
 import cv2
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import seaborn as sns
+try:
+    import matplotlib
+    matplotlib.use("Agg")
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as mpatches
+    import seaborn as sns
+    HAS_MATPLOTLIB = True
+except ImportError:
+    HAS_MATPLOTLIB = False
 
 import torch
 import torch.nn as nn
